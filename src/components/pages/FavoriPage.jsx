cd834e3af06c83937c9d1e9494144eabdbb338db
dianecasanova <festusnova1989@gmail.com>
2020-11-20 00:31:48 +0100

    improvements of cardgroup of favori_page

--- src/components/pages/FavoriPage.jsx
+++ src/components/pages/FavoriPage.jsx
@@ -2,11 +2,10 @@ import {
   CardGroup,
   Card,
   CardImg,
-  CardTitle,
   CardBody,
   CardSubtitle,
   CardText,
-  Button,
+  Badge,
 } from "reactstrap";
 import { useContext } from "react";
 
@@ -27,13 +26,19 @@ function FavoriPage() {
           alt="Card image cap"
         />
         <CardBody>
-          <CardTitle>{persons[0].name.first}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">
             {persons[0].name.first}
           </CardSubtitle>
           <CardText>
-            This card has supporting text below as a natural lead-in to
-            additional content.
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Musique
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Danse
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Perruques
+            </Badge>
           </CardText>
         </CardBody>
       </Card>
@@ -41,30 +46,192 @@ function FavoriPage() {
         <CardImg
           top
           width="100%"
-          src={persons[0].picture.medium}
+          src={persons[1].picture.medium}
           alt="Card image cap"
         />
         <CardBody>
-          <CardTitle tag="h5">{persons[0].name.first}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">
-            {persons[0].name.first}
+            {persons[1].name.first}
           </CardSubtitle>
           <CardText>
-            This card has supporting text below as a natural lead-in to
-            additional content.
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Théâtre
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Bal
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Equitation
+            </Badge>
           </CardText>
-          <Button>Button</Button>
         </CardBody>
       </Card>
       <Card>
         <CardImg
           top
           width="100%"
-          src={persons[0].picture.medium}
+          src={persons[2].picture.medium}
           alt="Card image cap"
         />
         <CardBody>
-          <CardTitle tag="h5">{persons[0].name.first}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {persons[2].name.first}
+          </CardSubtitle>
+          <CardText>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Frou-Frou
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Portrait
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Orgie
+            </Badge>
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src={persons[3].picture.medium}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {persons[3].name.first}
+          </CardSubtitle>
+          <CardText>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Musique
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Danse
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Perruques
+            </Badge>
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src={persons[4].picture.medium}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {persons[4].name.first}
+          </CardSubtitle>
+          <CardText>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Théâtre
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Bal
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Equitation
+            </Badge>
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src={persons[5].picture.medium}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {persons[5].name.first}
+          </CardSubtitle>
+          <CardText>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Frou-Frou
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Portrait
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Orgie
+            </Badge>
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src={persons[6].picture.medium}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {persons[6].name.first}
+          </CardSubtitle>
+          <CardText>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Musique
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Danse
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Perruques
+            </Badge>
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src={persons[7].picture.medium}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {persons[7].name.first}
+          </CardSubtitle>
+          <CardText>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Théâtre
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Bal
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Equitation
+            </Badge>
+          </CardText>
+        </CardBody>
+      </Card>
+      <Card>
+        <CardImg
+          top
+          width="100%"
+          src={persons[8].picture.medium}
+          alt="Card image cap"
+        />
+        <CardBody>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">
+            {persons[8].name.first}
+          </CardSubtitle>
+          <CardText>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Frou-Frou
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Portrait
+            </Badge>
+            <Badge pill color="dark" className="opacity-4 m-2 p-2">
+              Orgie
+            </Badge>
+          </CardText>
         </CardBody>
       </Card>
     </CardGroup>
