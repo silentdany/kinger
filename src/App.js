a3ce9e503c982846f50d29272978cf8dbddd13a1
a-dagnicourt <dagnicourt.a@gmail.com>
2020-11-19 11:33:58 +0100

    added dev branch and alert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 
+import { Alert } from 'reactstrap';
+
 function App() {
   return (
     <div className='App'>
       <h1>Kinger</h1>
       <h2>Tinder for the King</h2>
+      <Alert color='warning'>Branche dev, attention !</Alert>
     </div>
   );
 }
