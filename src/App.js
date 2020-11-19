4d0e4fa579a5dd7982df918dc696b7559ee00ecb
a-dagnicourt <dagnicourt.a@gmail.com>
2020-11-19 11:29:10 +0100

    initial setup (bootswatch, reactstrap, axios, fa, proptypes, routerdom)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Kinger</h1>
+      <h2>Tinder for the King</h2>
+    </div>
+  );
+}
+
+export default App;
