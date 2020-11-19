83ef310427f2d43b4d5e026a8284e9f4b31270ee
dianecasanova <festusnova1989@gmail.com>
2020-11-19 16:45:46 +0100

    Favori page in construction : add card of courtisans

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import FavoriPage from "./pages/FavoriPage";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kinger</h1>
+      <h2>Tinder for the King</h2>
+      <FavoriPage />
+    </div>
+  );
+}
+
+export default App;
