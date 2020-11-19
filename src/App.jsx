f9c5b81b4dd0166c6a5da2ddd6ab84a615a42b5e
Laura Glutron <lauraglutron@MacBook-Pro.local>
2020-11-19 15:03:40 +0100

    create carroussel, create info and description spaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css';
-
-import { Alert } from 'reactstrap';
+import "./App.css";
+import FocusPartisan from "./components/pages/FocusPartisan";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <h1>Kinger</h1>
       <h2>Tinder for the King</h2>
-      <Alert color='warning'>Branche dev, attention !</Alert>
+      <FocusPartisan />
     </div>
   );
 }
