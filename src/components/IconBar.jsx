2cfd28b3e87648687d7d7299a3d21ebc93563edf
dianecasanova <festusnova1989@gmail.com>
2020-11-20 03:33:47 +0100

    trying to put the feature addFav on the homepage from the iconbar

--- src/components/IconBar.jsx
+++ src/components/IconBar.jsx
@@ -1,10 +1,23 @@
 import { Button } from "reactstrap";
 import { ImCross } from "react-icons/im";
 import { BsFillHeartFill } from "react-icons/bs";
-import { AiFillStar } from "react-icons/ai";
 import "./IconBar.css";
+import { Link } from "react-router-dom";
+import { AiFillStar } from "react-icons/ai";
+import Axios from "axios";
+import { useEffect, useState } from "react";
 
 function IconBar() {
+  const [Favorited, setFavorited] = useState(false);
+
+  useEffect(() => {
+    Axios.get(
+      "https://randomuser.me/api/?results=20&nat=fr&gender=female&inc=name,dob,location,picture,id,login"
+    ).then((res) => {
+      setFavorited(res.data.results);
+    });
+  }, []);
+
   return (
     <div className="py-4 d-flex justify-content-center iconBar">
       <Button className="btn-outline-light rounded-circle h-auto w-auto iconButton">
@@ -13,9 +26,17 @@ function IconBar() {
       <Button className="btn-outline-light rounded-circle iconButton h-200">
         <BsFillHeartFill color="#24e5a9" size={25} />
       </Button>
-      <Button className="btn-outline-light rounded-circle h-auto w-auto iconButton">
-        <AiFillStar color="#20bbff" size={30} />
-      </Button>
+      <div className="py-4 d-flex justify-content-center iconBar">
+        <Link to="/fav">
+          <Button
+            className="btn-outline-light iconButton"
+            onClick={() => setFavorited((prev) => !prev)}
+          >
+            {Favorited ? " Add to Fav " : ""}
+            <AiFillStar color="#20bbff" size={30} />
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,6 +1,6 @@
-import React, { useContext, useState } from 'react';
-import UsersContext from '../contexts/UsersContext';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import UsersContext from "../contexts/UsersContext";
+import { Link } from "react-router-dom";
 import {
   Card,
   CardText,
@@ -13,28 +13,28 @@ import {
   CarouselIndicators,
   CarouselCaption,
   Badge,
-} from 'reactstrap';
-import { RiInformationFill } from 'react-icons/ri';
-import './Card.css';
+} from "reactstrap";
+import { RiInformationFill } from "react-icons/ri";
+import "./Card.css";
 
-import courtisane_1 from '../images/courtisane_1.jpg';
-import courtisane_2 from '../images/courtisane_2.jpg';
+import courtisane_1 from "../images/courtisane_1.jpg";
+import courtisane_2 from "../images/courtisane_2.jpg";
 
 // Profile pictures array for acrousel
 const items = [
   {
     src: courtisane_1,
-    altText: 'Slide 1',
-    caption: 'Slide 1',
-    header: 'Slide 1 Header',
-    key: '1',
+    altText: "Slide 1",
+    caption: "Slide 1",
+    header: "Slide 1 Header",
+    key: "1",
   },
   {
     src: courtisane_2,
-    altText: 'Slide 2',
-    caption: 'Slide 2',
-    header: 'Slide 2 Header',
-    key: '2',
+    altText: "Slide 2",
+    caption: "Slide 2",
+    header: "Slide 2 Header",
+    key: "2",
   },
 ];
 
@@ -68,11 +68,12 @@ function ProfileCard({ id }) {
   const slides = items.map((item) => {
     return (
       <CarouselItem
-        className='shadow-lg'
+        className="shadow-lg"
         onExiting={() => setAnimating(true)}
         onExited={() => setAnimating(false)}
-        key={item.src}>
-        <img src={item.src} alt={item.altText} style={{ height: '79vh' }} />
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} style={{ height: "79vh" }} />
         <CarouselCaption
           captionText={item.caption}
           captionHeader={item.caption}
@@ -83,7 +84,7 @@ function ProfileCard({ id }) {
 
   return (
     <div>
-      <Card style={{ maxHeight: '79vh', width: '100vw', border: '0' }}>
+      <Card style={{ maxHeight: "79vh", width: "100vw", border: "0" }}>
         <Carousel activeIndex={activeIndex} next={next} previous={previous}>
           <CarouselIndicators
             items={items}
@@ -93,28 +94,29 @@ function ProfileCard({ id }) {
           {/* Actual slide items */}
           {slides}
           <CarouselControl
-            style={{ display: 'none' }}
-            direction='prev'
-            directionText='Previous'
+            style={{ display: "none" }}
+            direction="prev"
+            directionText="Previous"
             onClickHandler={previous}
           />
           <CarouselControl
-            style={{ display: 'none' }}
-            direction='next'
-            directionText='Next'
+            style={{ display: "none" }}
+            direction="next"
+            directionText="Next"
             onClickHandler={next}
           />
         </Carousel>
         {/* Profile informations */}
         <CardBody
-          style={{ height: '79vh' }}
-          className='position-absolute d-flex justify-content-start align-items-end'>
-          <div className='d-flex flex-column'>
-            <div className='d-flex align-items-center'>
-              <CardTitle tag='h3' className='font-weight-bold'>
+          style={{ height: "79vh" }}
+          className="position-absolute d-flex justify-content-start align-items-end"
+        >
+          <div className="d-flex flex-column">
+            <div className="d-flex align-items-center">
+              <CardTitle tag="h3" className="font-weight-bold">
                 Josianne
               </CardTitle>
-              <CardSubtitle tag='h4' className='ml-2 font-weight-light'>
+              <CardSubtitle tag="h4" className="ml-2 font-weight-light">
                 42
               </CardSubtitle>
             </div>
@@ -122,22 +124,23 @@ function ProfileCard({ id }) {
               Icelui jovait clavecin debovt, Cela est se pevt brovtille por vovs
             </CardText>
             <CardText>
-              <Badge pill color='dark' className='opacity-4 m-2 p-2'>
+              <Badge pill color="dark" className="opacity-4 m-2 p-2">
                 Musique
               </Badge>
-              <Badge pill color='dark' className='opacity-4 m-2 p-2'>
+              <Badge pill color="dark" className="opacity-4 m-2 p-2">
                 Danse
               </Badge>
-              <Badge pill color='dark' className='opacity-4 m-2 p-2'>
+              <Badge pill color="dark" className="opacity-4 m-2 p-2">
                 Perruques
               </Badge>
             </CardText>
           </div>
           <div
-            className='d-flex w-25 justify-content-center'
-            style={{ zIndex: '500' }}>
-            <Link to='/partisan'>
-              <RiInformationFill size={30} style={{ fill: 'white' }} />
+            className="d-flex w-25 justify-content-center"
+            style={{ zIndex: "500" }}
+          >
+            <Link to="/partisan">
+              <RiInformationFill size={30} style={{ fill: "white" }} />
             </Link>
           </div>
         </CardBody>
