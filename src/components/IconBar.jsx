3ede71fec9c640cc7acf63f0609a49b383d0d449
dianecasanova <festusnova1989@gmail.com>
2020-11-20 11:05:08 +0100

    repare iconBar

--- src/components/IconBar.jsx
+++ src/components/IconBar.jsx
@@ -1,35 +1,21 @@
 import { Button } from "reactstrap";
 import { ImCross } from "react-icons/im";
 import { BsFillHeartFill } from "react-icons/bs";
-import "./IconBar.css";
-import { Link } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
-import { useContext, useEffect, useState } from "react";
-import UsersContext from "../contexts/UsersContext";
+import "./IconBar.css";
 
 function IconBar() {
-  const [Favorited, setFavorited] = useState(false);
-  const { persons } = useContext(UsersContext);
-
   return (
     <div className="py-4 d-flex justify-content-center iconBar">
-      <Button className="btn-outline-light rounded-circle iconButton mt-1">
+      <Button className="btn-outline-light rounded-circle h-auto w-auto iconButton">
         <ImCross color="#fe615f" size={25} />
       </Button>
-      <Button className="btn-outline-light rounded-circle iconButton heartIcon">
+      <Button className="btn-outline-light rounded-circle iconButton h-200">
         <BsFillHeartFill color="#24e5a9" size={25} />
       </Button>
-      <div className="py-4 d-flex justify-content-center iconBar">
-        <Link to="/fav">
-          <Button
-            className="btn-outline-light rounded-circle align-middle"
-            onClick={() => setFavorited((prev) => !prev)}
-          >
-            <AiFillStar color="#20bbff" size={30} />
-            {Favorited ? " Add to Fav " : ""}
-          </Button>
-        </Link>
-      </div>
+      <Button className="btn-outline-light rounded-circle h-auto w-auto iconButton">
+        <AiFillStar color="#20bbff" size={30} />
+      </Button>
     </div>
   );
 }
